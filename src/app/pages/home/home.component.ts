cb4d4019325c92bb06c2e88f47d7e48b94f996a3
BinaryLeo <leomouracss@fastmail.com>
2023-07-18 23:28:54 -0300

    FEAT: Cart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,20 @@
 import { Component } from "@angular/core";
-
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
 })
 export class HomeComponent {
   cols = 3;
-  category:string | undefined;
-  onColumnsCountChange(colsNum: number):void {
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
+
+  onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
-    console.log(colsNum)//*working out
+    console.log(colsNum); //*working out
   }
-  onShowCategory(newCategory : string):void{
-   this.category = newCategory;
-   console.log(newCategory)
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+    console.log(newCategory);
   }
 }
